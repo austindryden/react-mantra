fef9b886d713a17da3235ff11238902915383b6d
Austin Dryden <austinisnorobot@gmail.com>
2020-02-13 09:54:35 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {connect} from 'react-redux';
+import {createStore} from 'redux';
 
+const ADD_MANTRA='ADD_MANTRA';
+
+function addMantra(){
+  return {
+    type: ADD_MANTRA,
+    payload: {}
+  }
+}
+
+const defaultState = [];
+function mantra(state=defaultState, action){
+  let newState = state;
+  switch(action.type){
+    case ADD_MANTRA:
+      break;
+    default:
+      break;
+  }
+  return newState;
+
+}
+
+const store = createStore(mantra, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return (<React.Fragment>
+
+  </React.Fragment>);
 }
 
 export default App;
